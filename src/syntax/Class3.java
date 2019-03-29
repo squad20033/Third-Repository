e6dae04ca357b0586469b3eed597096bd2cd3476
squad20033 <squad20033@gmail.com>
2019-03-28 20:57:24 -0500

    Second Commit

--- src/syntax/Class3.java
+++ src/syntax/Class3.java
@@ -3,29 +3,17 @@ package syntax;
 public class Class3 {
 
 	public static void main(String[] args) {
-		/*String arr = "hollywood is so far";
-		System.out.println(arr.charAt(6));
-		System.out.println(arr.contains(" "));
-		System.out.println(arr.endsWith("far"));
-		System.out.println(arr.lastIndexOf("o"));
-		System.out.println(arr.concat(" away"));
-		System.out.println(arr.compareTo("hollywood"));
-		System.out.println(arr.replace('o', 'i'));
-		System.out.println(arr.substring(arr.length()/2));
-		System.out.println(arr.toUpperCase());
-		System.out.println(arr.toString());*/
+		/*String arr = "hollywood";
+		for (int i = (arr.length() - 1); i >= 0; i--) {
+			System.out.print(arr.charAt(i));*/
 		
+        /*String brr="Merhabalarr";
+        if (!brr.isEmpty()) {
+        	if (!((brr.length()%2==0)) && brr.length()>=3) {
+        		System.out.println(brr.charAt(brr.length()/2));
+        	}*/
 		
-		int[][] ar = { { 2, 6, 9, 3, 7 }, 
-				       { 1, 7, 9, 4, 8 }, 
-				       { 0, 4, 6, 2, 5 }, 
-				       { 0, 1, 2, 3, 9 } };
-		
-		for (int[] inner:ar) {
-			for(int numbers:inner) {
-				System.out.print(numbers+ " / ");
-			}
-			System.out.println("\n"+"-------------------");
-		}
+        }
 	}
-}
+
+
